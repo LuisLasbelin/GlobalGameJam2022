e214c6832aef8bd10a951d18ceffbf46eb313cc8
Jorge <jgragia@epsg.upv.es>
2022-01-28 19:20:21 +0100

    ObjetoSO y script del Salto

---
+++ Scripts/ObjetoSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Objeto nuevo", menuName ="Objeto")]
+public class ObjetoSO : ScriptableObject
+{
+    public string nombre;
+    public Sprite arte;
+}
